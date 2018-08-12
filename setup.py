9a9485f2f9b50085d4f9b08024dc94981af044f2
Lucas <lucas.duarte@rivendel.com.br>
2018-08-12 16:46:19 -0300

    Fixed Setup to install all requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
 
+
 here = path.abspath(path.dirname(__file__))
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
 
 setup(
     name = 'sg-analyzer',
     version='0.0.1',
     author='Lucas Duarte',
+    install_requires=install_requires,
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     #packages = ["sg-analyzer"],    
     entry_points={
